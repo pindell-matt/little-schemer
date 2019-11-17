4f4925ac5f569fc46d68266a3f7e8d16565dd03f
pindell-matt <pindell.m@protonmail.com>
2019-11-17 18:39:11 -0500

    add desc of rember

--- ch3-cons_the_magnificent.rkt
+++ ch3-cons_the_magnificent.rkt
@@ -26,3 +26,22 @@
  (my_rember 'toast '(bacon lettuce and tomato))
  '(bacon lettuce and tomato))
  
+;; official rember
+(define rember
+  (lambda (a lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+              ((eq? (car lat) a) (cdr lat))
+              (else (rember a
+                            (cdr lat))))))))
+
+(check-equal? ;; returns with 'bacon removed
+  (rember 'bacon '(bacon lettuce and tomato))
+  '(lettuce and tomato))
+
+;; my explanation of rember:
+;; rember iterates over the lat, checking if the next (car lat) matches the provided 'a' atom
+;; if it matches - it returns the rest of the list: (cdr lat)
+;; if it does not - it continues; passing the (cdr lat) into a new recursive call to rember: (rember (cdr lat))
+;; and, as always, since we're recursively calling rember - we always check first if the lat is null: (null? lat)
