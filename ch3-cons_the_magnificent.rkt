9bf81e7bd79be0740f7b07d8233be55fa490de6f
pindell-matt <pindell.m@protonmail.com>
2019-11-19 22:21:28 -0500

    add subst2

--- ch3-cons_the_magnificent.rkt
+++ ch3-cons_the_magnificent.rkt
@@ -239,3 +239,22 @@
   'fudge
   '(ice cream with fudge for dessert))
  '(ice cream with topping for dessert))
+
+;; subst2 - substitues eitehr the first occurrence of 'o1' _or_ 'o2' with 'new'
+(define subst2
+  (lambda (new o1 o2 lat)
+    (cond
+      ((null? lat) '())
+      (else (cond
+              ((or
+                (eq? (car lat) o1)
+                (eq? (car lat) o2))
+               (cons new (cdr lat)))
+              (else (cons (car lat)
+                          (subst2 new o1 o2
+                                  (cdr lat)))))))))
+
+(check-equal?
+ (subst2 'vanilla 'chocolate 'banana
+         '(banana ice cream with chocolate topping))
+ '(vanilla ice cream with chocolate topping))
