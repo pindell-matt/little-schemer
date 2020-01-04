5f7c17d8e9a04b8755306d10202df00ca7256a51
pindell-matt <pindell.m@protonmail.com>
2020-01-03 20:46:21 -0500

    add book-equan?

--- ch4-numbers_games.rkt
+++ ch4-numbers_games.rkt
@@ -336,3 +336,12 @@
 (check-true  (equan? 'a 'a))
 (check-false (equan? 'a 'b))
 (check-false (equan? 1 'b))
+
+(define book-equan?
+  (lambda (a1 a2)
+    (cond
+      ((and (number? a1) (number? a2))
+       (= a1 a2))
+      ((or (number? a1) (number? a2))
+       #f)
+      (else (eq? a1 a2)))))
