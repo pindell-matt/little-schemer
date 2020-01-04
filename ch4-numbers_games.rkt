bab642ee3c195079a7b29c751207486fd5116963
pindell-matt <pindell.m@protonmail.com>
2020-01-03 20:38:11 -0500

    add test for all-nums

--- ch4-numbers_games.rkt
+++ ch4-numbers_games.rkt
@@ -314,3 +314,7 @@
               (cons (car lat) (all-nums (cdr lat))))
               (else
                (all-nums (cdr lat))))))))
+
+(check-equal?
+ (all-nums '(5 pears 6 prunes 9 dates))
+ '(5 6 9))
