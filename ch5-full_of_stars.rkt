00843a888b5d33d700c6f84c0140199d5f51dc8d
pindell-matt <pindell.m@protonmail.com>
2020-02-01 23:41:59 -0500

    add my_insertR*

--- ch5-full_of_stars.rkt
+++ ch5-full_of_stars.rkt
@@ -30,4 +30,40 @@
   'sauce
   '(((tomato sauce) ((bean) sauce) (and ((flying)) sauce))))
  '(((tomato) ((bean)) (and ((flying))))))
-  
+
+;; book version
+(define rember*
+  (lambda (a l)
+    (cond
+      ((null? l) (quote ()))
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) a)
+          (rember* a (cdr l)))
+         (else
+          (cons (car l) (rember* a (cdr l))))))
+      (else (cons (rember* a (car l))
+                  (rember* a (cdr l)))))))
+
+
+(define my_insertR*
+  (lambda (new old l)
+    (cond
+      ((null? l) '())
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) old)
+          (cons old (cons new (my_insertR* new old (cdr l)))))
+         (else
+          (cons (car l) (my_insertR* new old (cdr l))))))
+      (else
+       (cons (my_insertR* new old (car l))
+             (my_insertR* new old (cdr l)))))))
+
+(check-equal?
+ (my_insertR*
+  'roast
+  'chuck
+  '((how much (wood)) could ((a (wood) chuck)) (((chuck))) (if (a) ((wood chuck))) could chuck wood))
+ '((how much (wood)) could ((a (wood) chuck roast)) (((chuck roast))) (if (a) ((wood chuck roast))) could chuck roast wood))
+ 
